4866ee997849a62390139079fbd39ce8209afe37
ele828 <dongjiemusic@qq.com>
2016-06-30 15:56:41 +0800

    implement frontend

---
+++ medium-plugin/lib/lib.js
@@ -0,0 +1,32 @@
+/**
+ * Medium-like Image Plugin
+ * Author: github.com/ele828
+ */
+
+var Plugin = (function() {
+  return {
+    loadImage: function(banner) {
+      var imgSmall = banner.querySelector('.img-small')
+      var img = new Image(),
+          width = imgSmall.dataset.width,
+          height = imgSmall.dataset.height;
+
+      img.src = imgSmall.src
+      img.addEventListener('load', (e) => {
+        imgSmall.classList.add('loaded')
+      }, false)
+
+      var imgLarge = new Image()
+
+      imgLarge.src = imgSmall.dataset.large
+      imgLarge.addEventListener('load', (e) => {
+        imgLarge.classList.add('loaded')
+      }, false)
+
+      imgSmall.parentNode.appendChild(imgLarge)
+    }
+  }
+})()
+
+var banners = document.querySelectorAll('.banner')
+Array.prototype.slice.apply(banners).forEach(Plugin.loadImage)
